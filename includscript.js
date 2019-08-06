c06b0ecb1521d1e4fda3e9ab420bbe4d1dadf858
houria chafiq <47454408+houria-ch@users.noreply.github.com>
2019-08-06 23:45:32 +0100

    Update includscript.js

--- includscript.js
+++ includscript.js
@@ -1,18 +1,16 @@
- (function() {
+(function() {
      var IDelement = document.getElementById('custplaceWidget');
-     if (IDelement) {
-
+     var marque = IDelement.getAttribute('data-id-marque'); //pour obtenir la valeur du data attribute.
+     if (IDelement && marque) {
          var frame = document.createElement('iframe');
-         frame.src = "https://houria-ch.github.io/projet-widget/";
+         frame.src = "https://houria-ch.github.io/widget/#" + marque;
          IDelement.appendChild(frame);
          frame.style.height = "195px";
          frame.style.paddingTop = "25px";
          frame.style.marginBottom = "-75px";
          frame.style.border = "none";
          frame.style.width = "100%";
-
      } else {
          console.log("error: id is incorrect");
-
      }
  })();
